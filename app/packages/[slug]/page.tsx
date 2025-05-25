dc438236f6e8c2c25e756e47abfafe4143540451
sriram806 <boddusriram1234@gmail.com>
2025-05-25 23:37:18 +0530

    one

--- app/packages/[slug]/page.tsx
+++ app/packages/[slug]/page.tsx
@@ -21,7 +21,7 @@ export async function generateStaticParams() {
   }));
 }
 
-type PageProps = {
+interface PageProps {
   params: Promise<{ slug: string }>;
   searchParams?: { [key: string]: string | string[] | undefined };
 }
